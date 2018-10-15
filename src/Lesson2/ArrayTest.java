b9600c340f1b221350395737d1e3543f15f13489
Vladim1r37 <vl4d1m1r37@gmail.com>
2018-10-15 17:50:46 +0300

    Тест методов добавления и удаления элементов массива, поиска элемента и замер скорости сортировки массива тремя способами.

--- src/Lesson2/ArrayTest.java
+++ src/Lesson2/ArrayTest.java
@@ -1,4 +1,46 @@
 package Lesson2;
 
+import java.util.concurrent.TimeUnit;
+
 public class ArrayTest {
+    public static void main(String[] args) {
+        // Создаю массив на 1000000 элементов внутри класса, работающего с массивом
+        ArrayManaged myArray = new ArrayManaged(1000000);
+
+        // Заполняю массив случайными числами
+        myArray.fillRandom(30000);
+
+        // Пытаюсь добавить новое значение в полный массив
+        System.out.println("Добавить число 100 в массив - " + myArray.add(100));
+
+        // Ищу число 100 в массиве
+        System.out.println("Есть ли число 100 в массиве - " + myArray.find(100));
+
+        // Такое число есть. Удаляю 100
+        System.out.println("Удалить число 100 из массива - " + myArray.remove(100));
+
+        // Добавляю число 100 в массив
+        System.out.println("Добавить число 100 в массив - " + myArray.add(100));
+
+        //Тест сортировки пузырьковым методом
+        System.out.println("sort bubble");
+        long start = System.nanoTime();
+        ArrayUtils.sortBubble(myArray.getData());
+        System.out.println("Время сортировки - " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start) + " миллисекунд");
+        myArray.fillRandom(30000);
+
+        //Тест сортировки методом выбора
+        System.out.println("sort select");
+        start = System.nanoTime();
+        ArrayUtils.sortSelect(myArray.getData());
+        System.out.println("Время сортировки - " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start) + " миллисекунд");
+        myArray.fillRandom(30000);
+
+        //Тест сортировки методом вставки
+        System.out.println("sort insert");
+        start = System.nanoTime();
+        ArrayUtils.sortInsert(myArray.getData());
+        System.out.println("Время сортировки - " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start) + " миллисекунд");
+
+    }
 }
