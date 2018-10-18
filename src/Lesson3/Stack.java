e25b88b326d8ae047967f5227007dcf48ee40c0b
Vladim1r37 <vl4d1m1r37@gmail.com>
2018-10-18 06:40:57 +0300

    реализован интерфейс стека для типа char

--- src/Lesson3/Stack.java
+++ src/Lesson3/Stack.java
@@ -1,4 +1,18 @@
 package Lesson3;
 
-public class Stack {
+public interface Stack {
+
+    void push(char value);
+
+    char pop();
+
+    char peek();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    int getSize();
+
+    void display();
 }
