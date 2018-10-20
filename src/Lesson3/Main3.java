880451aa4f98cd2a0e7b1d12091c7d1a06d0f1ce
Vladim1r37 <vl4d1m1r37@gmail.com>
2018-10-20 21:56:45 +0300

    добавлен метод тестирования программы работы с очередью

--- src/Lesson3/Main3.java
+++ src/Lesson3/Main3.java
@@ -5,6 +5,50 @@ public class Main3 {
 
         // тестируем BracketChecker
         testBracketChecker();
+
+        // работаем с очередью целых чисел
+        testQueueManager();
+    }
+
+    private static void testQueueManager() {
+        // создаем экземпляр очереди
+        QueueImpl queue = new QueueImpl(5);
+        // создаем экземпляр класса для безопасной работы с очередью
+        QueueManager queueManager = new QueueManager(queue);
+        // добавляем элементы в очередь
+        System.out.println("\n");
+        System.out.println(queueManager.insert(1));
+        System.out.println(queueManager.insert(2));
+        System.out.println(queueManager.insert(3));
+        System.out.println(queueManager.insert(4));
+        System.out.println(queueManager.insert(5));
+        // добавляем шестой элемент в очередь размером 5 элементов
+        System.out.println("\n");
+
+        System.out.println(queueManager.insert(6));
+        // удаляем элементы из очереди
+        System.out.println("\n");
+        System.out.println(queueManager.remove());
+        System.out.println(queueManager.remove());
+        System.out.println(queueManager.remove());
+        // добавляем еще несколько элементов
+        System.out.println("\n");
+        System.out.println(queueManager.insert(6));
+        System.out.println(queueManager.insert(7));
+        System.out.println(queueManager.insert(8));
+        System.out.println(queueManager.insert(9));
+        // выводим на экран очередь
+        System.out.println("\n");
+        queue.display();
+        // снова удаляем элементы, тестируем переход метки front в начало массива
+        System.out.println("\n");
+        System.out.println(queueManager.remove());
+        System.out.println(queueManager.remove());
+        System.out.println(queueManager.remove());
+        // проверяем метод чтения элемента, первого на выдачу в очереди
+        System.out.println("\n");
+        System.out.println(queue.peek());
+
     }
 
     private static void testBracketChecker() {
