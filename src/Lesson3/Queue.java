77c04a67b11fe890c94faec75254efdef5562b09
Vladim1r37 <vl4d1m1r37@gmail.com>
2018-10-20 18:51:40 +0300

    реализация интерфейса очереди

--- src/Lesson3/Queue.java
+++ src/Lesson3/Queue.java
@@ -1,4 +1,18 @@
 package Lesson3;
 
 public interface Queue {
+
+    void insert(int value);
+
+    int remove();
+
+    int peek();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    int getSize();
+
+    void display();
 }
