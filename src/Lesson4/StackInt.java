c83483ed18dfddd353c6328bb166b8231a46904d
Vladim1r37 <vl4d1m1r37@gmail.com>
2018-10-22 16:54:20 +0300

    в предыдущем уроке использовалась версия интерфейса с типом данных char

--- src/Lesson4/StackInt.java
+++ src/Lesson4/StackInt.java
@@ -1,4 +1,18 @@
 package Lesson4;
 
 public interface StackInt {
+
+    void push(int value);
+
+    int pop();
+
+    int peek();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    int getSize();
+
+    void display();
 }
