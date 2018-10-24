c85d1a1ae3b90c752fa0642e4143b89982b3819d
Vladim1r37 <vl4d1m1r37@gmail.com>
2018-10-24 19:22:13 +0300

    класс предмета из задачи о ранце

---
+++ src/Lesson5/Item.java
@@ -0,0 +1,37 @@
+package Lesson5;
+
+public class Item {
+
+    private String name;
+
+    private int weight;
+
+    private int cost;
+
+    public Item(String name, int weight, int cost) {
+        this.name = name;
+        this.weight = weight;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                ", cost=" + cost +
+                '}';
+    }
+}
