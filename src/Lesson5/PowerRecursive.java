d6c5505ff774554c237e11fe45845dac012a4af0
Vladim1r37 <vl4d1m1r37@gmail.com>
2018-10-26 18:18:26 +0300

    Добавлена обработка случаев, когда показатель отрицательный и равен нулю.

--- src/Lesson5/PowerRecursive.java
+++ src/Lesson5/PowerRecursive.java
@@ -4,10 +4,27 @@ public class PowerRecursive {
 
     public static void main(String[] args) {
 
-        System.out.println(powR(2, 8));
+        System.out.println(pow(2, 8));
+        System.out.println(pow(0, 0));
+        System.out.println(pow(0, 1));
+        System.out.println(pow(0, 3));
+        System.out.println(pow(2, -3));
+        System.out.println(pow(2, 0));
     }
 
-    private static int powR(int base, int exponent) {
+    private static int pow(int base, int exponent) {
+        if (base == 0 && exponent == 0)
+            System.out.println("Ошибка. 0 не возводится в степень 0");
+        else if (exponent < 0)
+            System.out.println("Ошибка. Показатель степени должен быть положительным.");
+        else if (exponent == 0)
+            return 1;
+        else
+            return powR(base, exponent);
+        return -1;
+    }
+
+    private static int powR (int base, int exponent) {
         if (exponent == 1)
             return base;
         return base * powR(base, exponent - 1);
